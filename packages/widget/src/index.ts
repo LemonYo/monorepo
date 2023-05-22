38a248997a4394b9be193249e88416484aae5120
web-ly <1411577366@qq.com>
2023-05-22 19:09:12 +0800

    eslint

--- packages/widget/src/index.ts
+++ packages/widget/src/index.ts
@@ -2,8 +2,4 @@ import utils from './utils'
 import CustomButton from './button'
 import Relative from './relative'
 
-export { 
-    utils, 
-    CustomButton,
-    Relative
- }
+export { utils, CustomButton, Relative }
